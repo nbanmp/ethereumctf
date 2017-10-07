8426de317132d79ffd65b6378a7e4513ff8f3be6
Nathan <nbanmp@gmail.com>
2017-10-06 20:54:22 -0400

    Server deploys and tests contracts now

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import request
 from flask import abort
+import setup as ethereumctf
 
 app = Flask(__name__)
 
@@ -12,16 +13,17 @@ def index():
 
 @app.route('/example', methods=['GET'])
 def example():
-    return "Here is the address you need to hack: "
+    return "Here is the address you need to hack: " + ethereumctf.deploy_new('example') + "\n"
 
 @app.route('/example', methods=['POST'])
 def example_test():
     if not request.args or not 'address' in request.args:
         abort(400)
     address_to_test = request.args['address']
-
-    # Check if the address was one we gave out and if it was successfully pwned.
-    return "Here is the address we are testing: " + request.args['address']
+    if(ethereumctf.check_address_for_victory(address_to_test)):
+        return "YOU WIN THE FLAG: eThCtF{flagsrock}\n"
+    else:
+        return "FAIL! No flags for you. :(\n"
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
--- setup.py
+++ setup.py
@@ -6,6 +6,18 @@ web3 = Web3(HTTPProvider('http://localhost:8545'))
 
 published_contracts = {}
 
+# Read solidity sources
+sources = {}
+with open('challenges/example/vulnerable.sol', 'r') as f:
+    sources['vulnerable.sol'] = {}
+    sources['vulnerable.sol']['content'] = f.read()
+
+# Compile Solidity
+contracts = compile_standard({
+    'language': 'Solidity',
+    'sources': sources
+})['contracts']
+
 def deploy_contract(contractFromSolc):
     abi = contractFromSolc['abi']
     code = contractFromSolc['evm']['bytecode']['object']
@@ -34,30 +46,24 @@ def check_example_for_victory(contract):
     print(contract.call().get())
     return contract.call().get() == 4
 
-# Read solidity sources
-sources = {}
-with open('challenges/example/vulnerable.sol', 'r') as f:
-    sources['vulnerable.sol'] = {}
-    sources['vulnerable.sol']['content'] = f.read()
+def deploy_new(challenge_name):
+    if(challenge_name == "example"):
+        # THIS IS THE PROCESS FOR DEPLOYING A CONTRACT AND ADDING IT TO THE published_contracts DICT:
+        # Select the contract
+        contractFromSolc = contracts['vulnerable.sol']['Vulnerable']
+        # Deploy the contract
+        deployed_contract = deploy_contract(contractFromSolc)
+        # Give it its own spot in published_contracts
+        published_contracts[deployed_contract.address] = {}
+        published_contracts[deployed_contract.address]['contract'] = deployed_contract
+        # Add a function to test to see if it has been solved
+        published_contracts[deployed_contract.address]['test_func'] = check_example_for_victory
 
-# Compile Solidity
-contracts = compile_standard({
-    'language': 'Solidity',
-    'sources': sources
-})['contracts']
-
-# THIS IS THE PROCESS FOR DEPLOYING A CONTRACT AND ADDING IT TO THE published_contracts DICT:
-# Select the contract
-contractFromSolc = contracts['vulnerable.sol']['Vulnerable']
-# Deploy the contract
-deployed_contract = deploy_contract(contractFromSolc)
-# Give it its own spot in published_contracts
-published_contracts[deployed_contract.address] = {}
-published_contracts[deployed_contract.address]['contract'] = deployed_contract
-# Add a function to test to see if it has been solved
-published_contracts[deployed_contract.address]['test_func'] = check_example_for_victory
+        return deployed_contract.address
 
 if __name__ == "__main__":
+    deployed_contract = published_contracts[deploy_new('example')]['contract']
+
     print(deployed_contract.call().getAddress())
 
     deployed_contract.transact({'from': '0x1da7e787a1897046677e57e87177c4de88cc388a'}).set(4, "0x1da7e787a1897046677e57e87177c4de88cc388a")
