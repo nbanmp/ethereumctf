b8a8a5e44bbf8cd562110463b4c0f1505179c3d2
Nathan <nbanmp@gmail.com>
2017-10-06 20:37:10 -0400

    added published_contracts object to save the contract and test function

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from solc import compile_standard
 
 web3 = Web3(HTTPProvider('http://localhost:8545'))
 
+published_contracts = {}
+
 def deploy_contract(contractFromSolc):
     abi = contractFromSolc['abi']
     code = contractFromSolc['evm']['bytecode']['object']
@@ -20,26 +22,46 @@ def deploy_contract(contractFromSolc):
 
     return contract_factory(address=contract_address)
 
+
+def check_address_for_victory(address):
+    global published_contracts
+    try:
+        return published_contracts[address]['test_func'](published_contracts[address]['contract'])
+    except KeyError as e:
+        return False
+
 def check_example_for_victory(contract):
     print(contract.call().get())
     return contract.call().get() == 4
 
+# Read solidity sources
 sources = {}
 with open('challenges/example/vulnerable.sol', 'r') as f:
     sources['vulnerable.sol'] = {}
     sources['vulnerable.sol']['content'] = f.read()
 
+# Compile Solidity
 contracts = compile_standard({
     'language': 'Solidity',
     'sources': sources
 })['contracts']
 
+# THIS IS THE PROCESS FOR DEPLOYING A CONTRACT AND ADDING IT TO THE published_contracts DICT:
+# Select the contract
 contractFromSolc = contracts['vulnerable.sol']['Vulnerable']
-
+# Deploy the contract
 deployed_contract = deploy_contract(contractFromSolc)
+# Give it its own spot in published_contracts
+published_contracts[deployed_contract.address] = {}
+published_contracts[deployed_contract.address]['contract'] = deployed_contract
+# Add a function to test to see if it has been solved
+published_contracts[deployed_contract.address]['test_func'] = check_example_for_victory
 
-print(deployed_contract.call().getAddress())
+if __name__ == "__main__":
+    print(deployed_contract.call().getAddress())
 
-deployed_contract.transact({'from': '0x1da7e787a1897046677e57e87177c4de88cc388a'}).set(2, "0x1da7e787a1897046677e57e87177c4de88cc388a")
+    deployed_contract.transact({'from': '0x1da7e787a1897046677e57e87177c4de88cc388a'}).set(4, "0x1da7e787a1897046677e57e87177c4de88cc388a")
 
-print(check_example_for_victory(deployed_contract))
+    print(check_address_for_victory(deployed_contract.address))
+
+    print(published_contracts)
